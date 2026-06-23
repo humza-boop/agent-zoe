e8878df5477bdd89958bfc4c057300147657a59a
Humza Saleem <humzasaleem@MacBookPro.attlocal.net>
2026-06-23 15:17:26 -0500

    Update Apps Script endpoint after column simplification redeploy

--- project/config.js
+++ project/config.js
@@ -1,3 +1,3 @@
 window.SURVEY_CONFIG = {
-  endpoint: 'https://script.google.com/macros/s/AKfycbzcCJysnDPQ_bgJLTLl9DAofktfVIe3o4XoZxdZkPxDFI_9sNEQzA59rZkJ4RaGo-iISw/exec',
+  endpoint: 'https://script.google.com/macros/s/AKfycbwTtzTM6FooNlzdrsIq9lahNg3gg0PE6mIt16NGNjXLK5Ou-aZu4VJvvrsgkf8yMZSECg/exec',
 };
